524858ef944a8985483cf4b017499fe2f1153879
Bohdan <bohdannikitiuk@gmail.com>
2023-10-21 18:56:00 +0300

    Home Task 3 complete

---
+++ lesson5_HT3/task3_2.js
@@ -0,0 +1,14 @@
+const number = 5
+let i = 1
+while (i <= 10){
+    console.log(`5 x ${i} = ${number*i}`)
+    i++
+}
+
+// для зворотнього порядку:
+// const number = 5
+// let i = 10
+// while (i > 0){
+  //console.log(`5 x ${i} = ${number*i}`)
+  //i--
+//}
