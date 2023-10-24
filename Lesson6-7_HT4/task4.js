239a360126f43a7ff8b31b4bbee7cb6288258b5a
Bohdan <bohdannikitiuk@gmail.com>
2023-10-24 15:27:10 +0300

    Fix2

--- Lesson6-7_HT4/task4.js
+++ Lesson6-7_HT4/task4.js
@@ -2,7 +2,7 @@ function divide(numerator, denominator){
         if(denominator === 0){
             throw new Error('Хто тебе генія вчив ділити на нуль? Повернися до школи, будь ласка')
         }
-        if(typeof numerator !== 'number' && typeof denominator !== 'number'){
+        if(typeof numerator !== 'number' || typeof denominator !== 'number'){
             throw new Error('Ну ти правда якийсь цікавий, як можна виконувати операцію ділення не з числом?')
         }
         return numerator / denominator
@@ -10,7 +10,7 @@ function divide(numerator, denominator){
     try{ 
         console.log(divide(10, 2))
         console.log(divide('pupa', 'lupa'))
-        //console.log(divide(10, 0))
+        console.log(divide(10, 0))
     }catch (e) {
         console.log(e.message)
     }
