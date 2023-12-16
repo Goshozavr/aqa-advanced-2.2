c772fb894a9768c27bd1cff983dd0cb53259b8a4
Bohdan <bohdannikitiuk@gmail.com>
2023-12-16 18:20:36 +0200

    Tome task 10 copleted

---
+++ lesson13_HT10/HT.js
@@ -0,0 +1,17 @@
+//task 1:
+"//h1";
+
+//task2:
+'*[@id="fruits"]';
+
+//task3:
+'//div[@id="colors"]//span[2]';
+//or:
+'//div[@id="colors"]//span[@class="color"][2]';
+
+//task4:
+'//td[text()="Alice"]/following-sibling::td';
+//or:
+'//td[text()="22"]';
+//or:
+'//table[@id="students"]//tr[3]//td[2]';
